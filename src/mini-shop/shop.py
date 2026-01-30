950417f2dc38ac2b7d93f3a5ebd08e32fa8875f7
Peter Grivalsky <pp.grivalsky@gmail.com>
2026-01-30 13:41:23 +0100

    add Cart class , product aggregation, total price

--- src/mini-shop/shop.py
+++ src/mini-shop/shop.py
@@ -3,19 +3,39 @@ class Product:
         self.sku = sku
         self.name = name
         self.price = price
-
         pass
 
     def __str__(self):
         return f"{self.name} (SKU: {self.sku}) –> {self.price} €"
         pass
 
+class Cart:
+    def __init__(self):
+        self.products = []
+        pass
 
+    def add_product(self, product):
+        self.products.append(product)
+        pass
+
+    def total_price(self):
+        total = 0
+        for product in self.products:
+            total += product.price        
+        return total
 
 def main():
-    p = Product(7255,"Pergola", 198)
-    print(p.name, p.price)
-    print(p)
+    p1 = Product(7255,"Pergola", 198)
+    p2 = Product(2724,"Wingarden", 378)
+    cart = Cart()
+    cart.add_product(p1)
+    cart.add_product(p2)
+
+    for product in cart.products:
+        print(product)
+    
+    print("Total price:", cart.total_price(), "€")
+    
 
 if __name__ == "__main__":
     main()
