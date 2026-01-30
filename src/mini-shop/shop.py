82b2fb2fa18ea75cff6a87b60e5ab87d49f43031
Peter Grivalsky <pp.grivalsky@gmail.com>
2026-01-30 13:30:55 +0100

    feat: add SKU and __str__ method

--- src/mini-shop/shop.py
+++ src/mini-shop/shop.py
@@ -1,12 +1,21 @@
 class Product:
-    def __init__(self, name):
+    def __init__(self, sku: str, name: str, price: float):
+        self.sku = sku
         self.name = name
+        self.price = price
+
+        pass
+
+    def __str__(self):
+        return f"{self.name} (SKU: {self.sku}) –> {self.price} €"
         pass
 
 
+
 def main():
-    p = Product("Pergola")
-    print(p.name)
+    p = Product(7255,"Pergola", 198)
+    print(p.name, p.price)
+    print(p)
 
 if __name__ == "__main__":
     main()
